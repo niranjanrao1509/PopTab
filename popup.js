e8b933e48be3f550ab4713245b1f5c6c4b178e99
Neeraj <neeraj.deshpande3@gmail.com>
2020-03-07 00:55:36 +0530

    Got the text

--- popup.js
+++ popup.js
@@ -1,22 +1,31 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-  }
-});
+// chrome.runtime.onMessage.addListener(function(request, sender) {
+//   if (request.action == "getSource") {
+//     message.innerText = request.source;
+//   }
+// });
 
-function onWindowLoad() {
+// function onWindowLoad() {
 
-  var message = document.querySelector('#message');
+//   var message = document.querySelector('#message');
 
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
+//   chrome.tabs.executeScript(null, {
+//     file: "getPagesSource.js"
+//   }, function() {
+//     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+//     if (chrome.runtime.lastError) {
+//       message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+//     }
+//   });
 
+// }
+
+// window.onload = onWindowLoad;
+function getSearchText(){
+  searchText=document.getElementById('searchBox').value;
+  // console.log(searchText);
+  window.alert(searchText);
 }
-
-window.onload = onWindowLoad;
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', getSearchText);
+ 
+});
